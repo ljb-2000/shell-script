2c5dee92765d85189e9062f04bae13b394d549fa
davidbj <david_bj@126.com>
2014-02-07 10:52:55 +0800

    restart Tomcat script update

--- restart_tomcat.sh
+++ restart_tomcat.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 #Author:David.Zhang
+#Date:2014-01-28
+#About:restart Tomcat service
 
-TOMCAT_PATH="/opt/data/tomcat"
-START_TOMCAT_SHELL="${TOMCAT_PATH}/bin/startup.sh"
-STOP_TOMCAT_SHELL="${TOMCAT_PATH}/bin/shutdown.sh"
+export JAVA_HOME=/opt/jdk1.6.0_35
+export TOMCAT_HOME=/opt/data/tomcat
 
-function stopTomcat {
-    $STOP_TOMCAT_SHELL 
-    if [[ $? -eq 0 ]];then
-        echo "停止成功!"
+function restartTomcat(){
+    echo "Attempting to find process id for Tomcat ..."
+    PID=`ps -ef |grep java| grep -v grep |awk {'print $2'}`
+
+    if [ "$PID" == "" ]
+    then
+        echo "Tomcat does not appear to be running ..."
     else
-        echo "停止失败!"
-        exit
+        echo "Killing Tomcat using process id for $PID"
+        kill -9 $PID
+        echo "Waiting for process $PID to end ..."
+        while `ps -ef | grep $PID | grep -v grep > /dev/null`;do
+            sleep 1
+        done
     fi
+
+    #delete Tomcat Cache
+    echo "Deleting Tomcat Cache..."
+    rm $TOMCAT_HOME/work/* -rf
+
+    #start Tomcat Service
+    echo "start Tomcat service..."
+    $TOMCAT_HOME/bin/startup.sh
 }
 
-function startTomcat {
-    $START_TOMCAT_SHELL
-    if [[ $? -eq 0 ]];then
-        echo "启动成功!"
-    else
-        echo "启动失败!"
-        exit
-    fi
-}
-
-function main {
-    ps aux | grep java | grep -v grep
-    if [[ $? -eq 0 ]];then
-        stopTomcat
-        sleep 5
-    fi
-    startTomcat
-}
-
-main
+restartTomcat
